5ca0d5d2cc6cc5e10daad447a2b5b3507bb47a99
hakki13420 <hak_rab@yahoo/fr>
2022-04-28 01:53:09 +0200

    add cookie alert

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const slidebar=document.querySelector('.sidebar')
 const elements= document.querySelectorAll('.sidebar-item a')
-console.log(elements)
+const cookies = document.querySelector('.cookies')
+const btnAccept=document.getElementById('accept-cook')
+
+
 const btns=document.querySelectorAll('.mode');
 btns.forEach(btn=>{
     btn.addEventListener('click',(e)=>{
@@ -16,4 +19,19 @@ btns.forEach(btn=>{
         slidebar.style.background=getComputedStyle(btn).background
 
     })
+})
+
+
+//onload
+window.onload=()=>{
+    setTimeout(() => {        
+        cookies.style.display="flex"
+        cookies.style.bottom=0
+    }, 3000);
+}
+
+
+// click accept
+btnAccept.addEventListener('click',()=>{
+    cookies.style.display="none"
 })
